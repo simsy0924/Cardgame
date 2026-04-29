57655aab72d8ce8ec6ce3c1e278a2b30d7f664ce
simsy0924 <simsy0924@gmail.com>
2026-04-29 18:21:29 +0900

    Delete js/oldone.js

--- js/oldone.js
+++
@@ -1,7 +0,0 @@
-// oldone.js — 올드 원 테마 효과 라우팅
-['올드 원', '올드원'].forEach(theme => {
-  registerThemeEffectHandler(theme, {
-    activateFromHand: activateThemeCardEffectFromHand,
-    resolveLink: resolveThemeEffectGeneric,
-  });
-});
